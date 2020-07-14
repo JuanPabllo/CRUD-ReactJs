4acd8b281c2a4654b0cc6a1d48e9b1adcab25875
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-07-14 15:27:33 -0300

    Content component

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default (props) => (
+    <Main
+        icon="home"
+        title="Início"
+        subtitle="Segundo Projeto do capitulo de React"
+    >
+        <div className="display-4">Bem vindo</div>
+        <hr />
+        <p className="mb-0">
+            Sistema para exemplificar a construção de um cadastro desenvolvido
+            em React!
+        </p>
+    </Main>
+);
