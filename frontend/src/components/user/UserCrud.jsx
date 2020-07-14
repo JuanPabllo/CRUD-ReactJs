426f8db7a5940dfdb37767ff63d87a236882568c
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-07-14 17:25:52 -0300

    Componente cadastro add UserCurd

---
+++ frontend/src/components/user/UserCrud.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Main from '../template/Main'
+
+const HeaderProps = {
+    icon: 'users',
+    title: 'Usuários',
+    subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir!'
+}
+
+export default class UserCrud extends Component {
+    
+}
