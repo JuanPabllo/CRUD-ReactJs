e36d0b4b37f75fe31a6ffe2e4b35a996331a80c9
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-07-14 15:13:03 -0300

    nav compente

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,4 +1,15 @@
 import "./Nav.css";
 import React from "react";
 
-export default (props) => <aside className="menu-area">Navegação</aside>;
+export default (props) => (
+    <aside className="menu-area">
+        <nav className="menu">
+            <a href="#/">
+                <i className="fa fa-home">Início</i>
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users">Usuários</i>
+            </a>
+        </nav>
+    </aside>
+);
