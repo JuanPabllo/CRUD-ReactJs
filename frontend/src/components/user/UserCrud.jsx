8ad11bb1a88d6d0390946e04c1c9c1676dd266eb
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-07-15 14:39:00 -0300

    login component

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -1,17 +1,108 @@
-import React, { Component } from "react";
-import Main from "../template/Main";
+import React, { Component } from 'react'
+import axios from 'axios'
+import Main from '../template/Main'
 
-const HeaderProps = {
-    icon: "users",
-    title: "Usuários",
-    subtitle: "Cadastro de usuários: Incluir, Listar, Alterar e Excluir!",
-};
+const headerProps = {
+    icon: 'users',
+    title: 'Usuários',
+    subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir!'
+}
+
+const baseUrl = 'http://localhost:3001/users'
+const initialState = {
+    user: { name: '', email: '' },
+    list: []
+}
 
 export default class UserCrud extends Component {
-    render () {
+
+    state = { ...initialState }
+
+    componentWillMount() {
+        axios(baseUrl).then(resp => {
+            this.set
+        })
+    }
+
+    clear() {
+        this.setState({ user: initialState.user })
+    }
+
+    save() {
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post'
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+        axios[method](url, user)
+            .then(resp => {
+                const list = this.getUpdatedList(resp.data)
+                this.setState({ user: initialState.user, list })
+            })
+    }
+
+    getUpdatedList(user, add = true) {
+        const list = this.state.list.filter(u => u.id !== user.id)
+        if(add) list.unshift(user)
+        return list
+    }
+
+    updateField(event) {
+        const user = { ...this.state.user }
+        user[event.target.name] = event.target.value
+        this.setState({ user })
+    }
+
+    renderForm() {
         return (
-            <Main {...HeaderProps}>
-                Cadastro de Usuário
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input type="text" className="form-control"
+                                name="name"
+                                value={this.state.user.name}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o nome..." />
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>E-mail</label>
+                            <input type="text" className="form-control"
+                                name="email"
+                                value={this.state.user.email}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o e-mail..." />
+                        </div>
+                    </div>
+                </div>
+
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary"
+                            onClick={e => this.save(e)}>
+                            Salvar
+                        </button>
+
+                        <button className="btn btn-secondary ml-2"
+                            onClick={e => this.clear(e)}>
+                            Cancelar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+   
+    
+    render() {
+        return (
+            <Main {...headerProps}>
+                {this.renderForm()}
+                
             </Main>
         )
     }
